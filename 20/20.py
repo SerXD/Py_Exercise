af58c3ce533d95bcbe960c95d35566a7fe221780
SerXD <serpbot.007@gmail.com>
2022-09-22 23:24:12 +0700

    update 19

--- 20/20.py
+++ 20/20.py
@@ -1,7 +1,7 @@
 shopping = int(input('Shopping: '))
 hour = int(input('Hour: '))
 minute = int(input('Minute: '))
-if (shopping < 0 or hour < 0 or minute < 0) :
+if shopping or hour or minute < 0 :
     print("It can't be negative.")
 else:
     price = 0
@@ -13,4 +13,7 @@ else:
         hour+=1
     for index in range(hour):
         price+=30
-    print('Price Parking: {} bath.'.format(price))
+    if price >= 160:
+        print('Price Parking: 150 bath.')
+    else:
+        print('Price Parking: {} bath.'.format(price))
