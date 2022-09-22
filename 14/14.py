249a011b45c7da89f6e1e4ec705aab7607672061
SerXD <serpbot.007@gmail.com>
2022-09-22 22:30:26 +0700

    update 14

---
+++ 14/14.py
@@ -0,0 +1,8 @@
+number_1 = int(input('Number 1: '))
+number_2 = int(input('Number 2: '))
+if number_1+number_2 == 0:
+    print('a + b = 0')
+elif number_1+number_2 > 0:
+    print('a + b > 0')
+else:
+    print('a + b < 0')
