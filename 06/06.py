704141d4c34458c91898889366b40ffc9f4856a2
SerXD <serpbot.007@gmail.com>
2022-09-22 20:36:45 +0700

    update 06

---
+++ 06/06.py
@@ -0,0 +1,8 @@
+number = int(input('Int: '))
+if number < 0 :
+    print('Please insert the number that is greater or equal zero.')
+else:
+    if number >= 50:
+        print('Pass.')
+    else:
+        print('Fail.')
